86e68b3e0a20da4d7b28f337ce8054ba3b2fc378
Mumtaz Athiera <mumtazathiera732@gmail.com>
2025-07-21 16:32:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-header("location: routes/web.php" );
+define('LARAVEL_START', microtime(true));
+
+// Make sure this path matches your Laravel folder name!
+require __DIR__ . '/../MediQTrack2/vendor/autoload.php';
+$app = require_once __DIR__ . '/../MediQTrack2/bootstrap/app.php';
+
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
